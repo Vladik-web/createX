c7a5807eb1d9680d1fb2cddf7a4e84e8734cdfcd
Vlad <seredavlad40@gmail.com>
2021-11-16 15:14:03 +0200

    Desktop done

--- js/common.js
+++ js/common.js
@@ -6,17 +6,66 @@ document.addEventListener('DOMContentLoaded', function() {
     dots: false,
     prevArrow: $('.slider__prev'),
     nextArrow: $('.slider__next'),
-    dotsClass: 'slider__pages'
+    dotsClass: 'slider__pages',
+    responsive: [{
+      breakpoint: 1500,
+      settings: {
+        arrows: false,
+
+
+      }
+    }]
   });
   $('.sec-slide').slick({
     arrows: true,
     dots: false,
     slidesToShow: 3,
-    autoplay: true,
+    autoplay: false,
     prevArrow: $('.sec-slide__left'),
     nextArrow: $('.sec-slide__right'),
+    responsive: [{
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+
+
+      }
+    }, {
+      breakpoint: 576,
+      settings: {
+        slidesToShow: 1,
+
+
+      }
+    }]
+
+  });
+  $('.reviews-slider').slick({
+    arrows: true,
+    dots: false,
+    slidesToShow: 1,
+    autoplay: true,
+
+    prevArrow: $('.reviews-left'),
+    nextArrow: $('.reviews-right'),
+    asNavFor: '.reviews-photo'
+  });
+  $('.reviews-photo').slick({
+    asNavFor: '.reviews-slider',
+    dots: false,
+    slidesToShow: 1,
+    autoplay: true,
+    prevArrow: $('.reviews-left'),
+    nextArrow: $('.reviews-right'),
 
   });
 
+
+  const headerBurgerBtn = document.querySelector('.header__burger-btn');
+  const navList = document.querySelector('.nav__list');
+  headerBurgerBtn.addEventListener('click', () => {
+    navList.classList.toggle('active');
+    document.body.classList.toggle('active');
+  })
 
 })
